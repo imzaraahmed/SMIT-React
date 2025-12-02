beff06b475a7cdd12b6a18d9435fc8b03ff670c3
imzaraahmed <imzarajamal@gmail.com>
2025-12-02 23:18:07 +0500

    create a component and apply css

---
+++ my-app/src/components/Myinfo.js
@@ -0,0 +1,15 @@
+import './Myinfo.css';
+
+function Myinfo(){
+    return(
+       <>
+       <div className="myinfo">
+       <h1>My Introduction</h1>
+       <p>My name is zara</p>
+       <p>I am 30 years old</p>
+       <p>Enrolled in BSCS at Virtual University and Smit in Web development course.</p>
+       </div>
+       </>
+    );
+}
+export default Myinfo;
